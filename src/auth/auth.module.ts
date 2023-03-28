2489c0318bc2c7524710fbdea0f79f5465d6528c
bruuheem <b.hallouchadjebbour@esi-sba.dz>
2023-03-28 17:03:32 +0100

    auth ++

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PrismaService } from 'src/prisma.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService,PrismaService, LocalStrategy],
+    imports: [UsersModule, PassportModule],
+    providers: [AuthService, PrismaService, LocalStrategy],
+    controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { UpdateUserDto } from '../users/dto/update-user.dto'
+import { UpdateUserDto } from '../users/dto/update-user.dto';
 import { UserQueryDto } from '../users/dto/user-query.dto';
 import { UserNotFoundException } from '../users/exceptions/userNotFound.exception';
 
-
 @Injectable()
 export class AuthService {
- constructor(private readonly prismaServive: PrismaService) {}
-  
+    constructor(private readonly prismaServive: PrismaService) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.findOne(email);
-    if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = await this.findOne(email);
+        if (user && user.password === password) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
     }
-    return null;
-  }
 
-  async findOne(email: string) {
-    const user = await this.prismaServive.user.findUnique({
-        where: { email },
-    });
+    async findOne(email: string) {
+        const user = await this.prismaServive.user.findUnique({
+            where: { email },
+        });
 
-    return user;
-}
+        return user;
+    }
 }
