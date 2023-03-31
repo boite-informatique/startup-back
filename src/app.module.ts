86b711cb9d6fe4e7cd332a22efa0668d39297862
AMINEBENCHAA <ma.benchaa@esi-sba.dz>
2023-03-31 01:54:43 +0100

    roleApi corrected - permissions module deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
-import { PermissionsModule } from './permissions/permissions.module';
+
 
 @Module({
-    imports: [UsersModule, RolesModule, PermissionsModule],
+    imports: [UsersModule, RolesModule],
 })
 export class AppModule {}
--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,14 +1,10 @@
-import { User, Permission } from '@prisma/client';
-import { Type } from 'class-transformer';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { CreatePermissionDto } from 'src/permissions/dto/create-permission.dto';
+
 
 import {
     IsInt,
-    IsNotEmpty,
-    IsOptional,
+   
     IsString,
-    ValidateNested,
+   
 } from 'class-validator';
 
 export class CreateRoleDto {
