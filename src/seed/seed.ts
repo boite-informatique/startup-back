a844621aea24db315f6c5702be2bbfb41bf89f9b
AMINEBENCHAA <ma.benchaa@esi-sba.dz>
2023-03-31 01:34:10 +0100

    first seed

---
+++ src/seed/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker'
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Generate mock data using the faker library
+  function GenerateUser(){
+   return {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    middle_name: faker.name.middleName(),
+    date_of_birth: faker.date.birthdate(),
+    location_of_birth: faker.address.city(),
+    sex: faker.helpers.arrayElement(["Male","Female"]),
+    type: faker.helpers.arrayElement(["Teacher","Student","Staff"]),}
+  };
+
+  function GenerateUsers(num:number){
+   const users=[]
+   for (let i=0;i<num;i++) {
+     users.push(GenerateUser())
+   }
+   return users;
+  }
+const users = GenerateUsers(10)
+console.log(users)
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
