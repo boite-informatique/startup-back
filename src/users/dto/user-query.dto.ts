34c76b89ffd5c95b585c1b175a3b1b7b70ce0ca2
ricin9 <ricin9@outlook.com>
2023-04-02 16:13:40 +0100

    changed a linting rule

--- src/users/dto/user-query.dto.ts
+++ src/users/dto/user-query.dto.ts
@@ -1,4 +1,4 @@
-import { Max, IsOptional, IsIn, IsInt, Min, IsString } from 'class-validator';
+import { Max, IsIn, IsInt, Min, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { UserType } from '.prisma/client';
 import { PartialType } from '@nestjs/swagger';
