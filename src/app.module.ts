1efdd49b9898a4bcfd0ea825c456697c9401acbe
ricin9 <ricin9@outlook.com>
2023-04-02 16:03:19 +0100

    resolved merge conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { JwtAuthGuard } from './auth/jwt-auth/jwt-auth.guard';
 
 @Module({
     imports: [UsersModule, AuthModule, PermissionsModule, RolesModule],
