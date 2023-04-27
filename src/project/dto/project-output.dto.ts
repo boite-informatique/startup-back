35e724b5edf02c7080af9b477a2f677e1dca0da4
ricin9 <ricin9@outlook.com>
2023-04-27 11:14:15 +0100

    fixed thing

--- src/project/dto/project-output.dto.ts
+++ src/project/dto/project-output.dto.ts
@@ -4,8 +4,8 @@ export class Project {
     resume: string;
     brand_name: string;
     product_name: string;
-    owner_id : number;
-    supervisor_id : number;
-    co_supervisor_id:number;
-    membersId : number[];
+    owner_id: number;
+    supervisor_id: number;
+    co_supervisor_id: number;
+    // membersId : number[];
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -8,40 +8,38 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProjectService {
-  constructor(
-    private readonly prismaService: PrismaService
-  ) {}
+    constructor(private readonly prismaService: PrismaService) {}
 
-  async create(data: CreateProjectDto): Promise<Project> {
-    const members = await this.prismaService.user.findMany({
-      where: {
-        id: {
-          in: data.membersId,
-        },
-      },
-    });
+    async create(data: CreateProjectDto): Promise<Project> {
+        const members = await this.prismaService.user.findMany({
+            where: {
+                id: {
+                    in: data.membersId,
+                },
+            },
+        });
 
-    const project = await this.prismaService.project.create({
-      data: {
-        type: data.type,
-        resume: data.resume,
-        brand_name: data.brand_name,
-        product_name: data.product_name,
-        owner: {
-          connect: { id: data.owner_id },
-        },
-        supervisor: {
-          connect: { id: data.supervisor_id },
-        },
-        co_supervisor: {
-          connect: { id: data.co_supervisor_id },
-        },
-        membersId: {
-          connect: members.map((member) => ({ id: member.id })),
-        },
-      },
-    });
+        const project = await this.prismaService.project.create({
+            data: {
+                type: data.type,
+                resume: data.resume,
+                brand_name: data.brand_name,
+                product_name: data.product_name,
+                owner: {
+                    connect: { id: data.owner_id },
+                },
+                supervisor: {
+                    connect: { id: data.supervisor_id },
+                },
+                co_supervisor: {
+                    connect: { id: data.co_supervisor_id },
+                },
+                members: {
+                    connect: members.map((member) => ({ id: member.id })),
+                },
+            },
+        });
 
-    return project;
-  }
+        return project;
+    }
 }
