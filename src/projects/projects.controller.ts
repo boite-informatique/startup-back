052f65d0111cbf884ecc575f310ef966c4793694
ricin9 <ricin9@outlook.com>
2023-05-06 16:18:26 +0100

    work on project api in progress

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Controller('projects')
 export class ProjectsController {
@@ -8,5 +9,10 @@ export class ProjectsController {
     @Get()
     async getProjects(@Request() req) {
         return await this.projectsService.getProjects(req.user);
+    }
+
+    @Post()
+    async createProject(@Request() req, @Body() body: CreateProjectDto) {
+        return await await this.projectsService.createProject(req.user, body);
     }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,5 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+    BadRequestException,
+    ConflictException,
+    ForbiddenException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Injectable()
 export class ProjectsService {
@@ -9,13 +16,61 @@ export class ProjectsService {
         let projects = [];
         if (user.type == 'student') {
             projects = await this.prismaService.project.findMany({
-                where: { members: { some: { id: user.id } } },
+                where: { members: { some: { id: user.sub } } },
                 take: 1,
             });
         } else {
             projects = await this.prismaService.project.findMany({
-                where: { OR: {} },
+                where: {
+                    OR: {
+                        owner_id: user.sub,
+                        supervisors: { some: { id: user.sub } },
+                    },
+                },
             });
         }
+
+        if (projects.length == 0)
+            throw new NotFoundException('No projects found');
+        return projects;
+    }
+
+    async createProject(user: any, body: CreateProjectDto) {
+        if (user.type == 'student') {
+            body.members.push(user.email);
+            if (body.members.length > 6)
+                throw new BadRequestException(
+                    'Project members cannot be more than 6',
+                );
+            if (
+                (await this.prismaService.project.count({
+                    where: { members: { some: { id: user.sub } } },
+                })) > 0
+            )
+                throw new ConflictException(
+                    'Students cannot have more than one project',
+                );
+        }
+
+        if (user.type == 'staff') {
+            throw new ForbiddenException('You cannot create projects');
+        }
+
+        console.log(body);
+
+        return await this.prismaService.project.create({
+            data: {
+                brand_name: body.brand_name,
+                product_name: body.product_name,
+                resume: body.resume,
+                logo: body?.logo,
+                type: body.type,
+                owner: { connect: { id: user.sub } },
+                members: { connect: body.members.map((email) => ({ email })) },
+                supervisors: {
+                    connect: body.supervisors.map((email) => ({ email })),
+                },
+            },
+        });
     }
 }
