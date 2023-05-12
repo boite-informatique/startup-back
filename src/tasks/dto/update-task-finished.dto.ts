c40fc9cb619e660adcf29865a4ca4a56e170d346
Amine Benchaa <ma.benchaa@esi-sba.dz>
2023-05-12 23:50:45 +0200

    task finished first release

---
+++ src/tasks/dto/update-task-finished.dto.ts
@@ -0,0 +1,6 @@
+import { OmitType, PartialType } from "@nestjs/mapped-types";
+import { CreateTaskFinishedDto } from "./create-task-finished.dto";
+
+export class UpdateTaskFinishedDto extends PartialType(
+    OmitType(CreateTaskFinishedDto, ['taskId']),
+) {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,16 +9,19 @@ import {
     Req,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TasksFinishedService } from './tasks-finished.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { Comment } from '@prisma/client';
+import { CreateTaskFinishedDto } from './dto/create-task-finished.dto';
+import { UpdateTaskFinishedDto } from './dto/update-task-finished.dto';
 
 @ApiTags('project tasks')
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly tasksService: TasksService) {}
-
+    constructor(private readonly tasksService: TasksService , private readonly tasksFinishedService: TasksFinishedService) {}
+  
     @Post()
     create(@Body() createTaskDto: CreateTaskDto, @Req() user: any) {
         return this.tasksService.create(createTaskDto, user.sub);
@@ -40,5 +43,21 @@ export class TasksController {
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.tasksService.remove(+id);
+    }
+    @Post(':id/finished')
+    createTaskFinished(@Body() createTaskFinishedDto: CreateTaskFinishedDto) {
+        return this.tasksFinishedService.create(createTaskFinishedDto)
+    }
+    @Get(':id/finished')
+    findTaskFinished(@Param('id') id: string){
+        return this.tasksFinishedService.find(+id)
+    }
+    @Patch(':id/finished')
+    updateteTaskFinished(@Param('id') id: string,@Body() updateTaskFinishedDto: UpdateTaskFinishedDto) {
+        return this.tasksFinishedService.update(+id,updateTaskFinishedDto)
+    }
+    @Delete(':id/finished')
+    deleteTaskFinished(@Param('id') id: string){
+        return this.tasksFinishedService.delete(+id)
     }
 }
